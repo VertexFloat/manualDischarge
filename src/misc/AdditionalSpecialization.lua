343bbe72c984bfc261644cc61edb1c5726f13109
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2023-06-04 17:35:20 +0200

    Small changes

--- src/misc/AdditionalSpecialization.lua
+++ src/misc/AdditionalSpecialization.lua
@@ -1,5 +1,5 @@
 -- @author: 4c65736975, All Rights Reserved
--- @version: 1.0.0.0, 07/07/2022
+-- @version: 1.0.0.0, 07|07|2022
 -- @filename: AdditionalSpecialization.lua
 
 local modName = g_currentModName
@@ -8,10 +8,10 @@ local function finalizeTypes(self)
 	if self.typeName == 'vehicle' then
 		for typeName, typeEntry in pairs(self:getTypes()) do
 			if SpecializationUtil.hasSpecialization(Pipe, typeEntry.specializations) and SpecializationUtil.hasSpecialization(Dischargeable, typeEntry.specializations) then
-				local additionalSpecialization = modName .. '.manualPipeDischargeable'
+				local specialization = modName .. '.manualPipeDischargeable'
 
-				if not SpecializationUtil.hasSpecialization(additionalSpecialization, typeEntry.specializations) then
-					self:addSpecialization(typeName, additionalSpecialization)
+				if not SpecializationUtil.hasSpecialization(specialization, typeEntry.specializations) then
+					self:addSpecialization(typeName, specialization)
 				end
 			end
 		end

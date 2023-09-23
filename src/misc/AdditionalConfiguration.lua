b1db7c2c6d74b14bfc45c2f6a42b9b4c219ae31d
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2023-09-23 23:18:17 +0200

    improved code

--- src/misc/AdditionalConfiguration.lua
+++ src/misc/AdditionalConfiguration.lua
@@ -3,43 +3,43 @@
 -- @filename: AdditionalConfiguration.lua
 
 local function getConfigurationsFromXML(xmlFile, superFunc, key, baseDir, customEnvironment, isMod, storeItem)
-	local configurations, defaultConfigurationIds = superFunc(xmlFile, key, baseDir, customEnvironment, isMod, storeItem)
-	local hasAutoAim = xmlFile:getValue('vehicle.pipe.pipeConfigurations.pipeConfiguration(0).pipeNodes.pipeNode(0)#autoAimYRotation', false) or xmlFile:getValue('vehicle.pipe.pipeNodes.pipeNode(0)#autoAimYRotation', false)
+  local configurations, defaultConfigurationIds = superFunc(xmlFile, key, baseDir, customEnvironment, isMod, storeItem)
+  local hasAutoAim = xmlFile:getValue("vehicle.pipe.pipeConfigurations.pipeConfiguration(0).pipeNodes.pipeNode(0)#autoAimYRotation", false) or xmlFile:getValue("vehicle.pipe.pipeNodes.pipeNode(0)#autoAimYRotation", false)
 
-	if xmlFile:hasProperty('vehicle.pipe') and not hasAutoAim then
-		if configurations == nil then
-			configurations = {}
-		end
+  if xmlFile:hasProperty("vehicle.pipe") and not hasAutoAim then
+    if configurations == nil then
+      configurations = {}
+    end
 
-		if defaultConfigurationIds == nil then
-			defaultConfigurationIds = {}
-		end
+    if defaultConfigurationIds == nil then
+      defaultConfigurationIds = {}
+    end
 
-		configurations.manualDischarge = {
-			{
-				isDefault = true,
-				saveId = '1',
-				isSelectable = true,
-				index = 1,
-				dailyUpkeep = 0,
-				price = 0,
-				name = g_i18n:getText('configuration_valueNo'),
-				nameCompareParams = {}
-			},
-			{
-				isDefault = false,
-				saveId = '2',
-				isSelectable = true,
-				index = 2,
-				dailyUpkeep = 0,
-				price = 0,
-				name = g_i18n:getText('configuration_valueYes'),
-				nameCompareParams = {}
-			}
-		}
-	end
+    configurations.manualDischarge = {
+      {
+        isDefault = true,
+        saveId = "1",
+        isSelectable = true,
+        index = 1,
+        dailyUpkeep = 0,
+        price = 0,
+        name = g_i18n:getText("configuration_valueNo"),
+        nameCompareParams = {}
+      },
+      {
+        isDefault = false,
+        saveId = "2",
+        isSelectable = true,
+        index = 2,
+        dailyUpkeep = 0,
+        price = 0,
+        name = g_i18n:getText("configuration_valueYes"),
+        nameCompareParams = {}
+      }
+    }
+  end
 
-	return configurations, defaultConfigurationIds
+  return configurations, defaultConfigurationIds
 end
 
 StoreItemUtil.getConfigurationsFromXML = Utils.overwrittenFunction(StoreItemUtil.getConfigurationsFromXML, getConfigurationsFromXML)
--- src/misc/AdditionalSpecialization.lua
+++ src/misc/AdditionalSpecialization.lua
@@ -5,17 +5,17 @@
 local modName = g_currentModName
 
 local function finalizeTypes(self)
-	if self.typeName == 'vehicle' then
-		for typeName, typeEntry in pairs(self:getTypes()) do
-			if SpecializationUtil.hasSpecialization(Pipe, typeEntry.specializations) and SpecializationUtil.hasSpecialization(Dischargeable, typeEntry.specializations) then
-				local specialization = modName .. '.manualPipeDischargeable'
+  if self.typeName == "vehicle" then
+    for typeName, typeEntry in pairs(self:getTypes()) do
+      if SpecializationUtil.hasSpecialization(Pipe, typeEntry.specializations) and SpecializationUtil.hasSpecialization(Dischargeable, typeEntry.specializations) then
+        local specialization = modName .. ".manualPipeDischargeable"
 
-				if not SpecializationUtil.hasSpecialization(specialization, typeEntry.specializations) then
-					self:addSpecialization(typeName, specialization)
-				end
-			end
-		end
-	end
+        if not SpecializationUtil.hasSpecialization(specialization, typeEntry.specializations) then
+          self:addSpecialization(typeName, specialization)
+        end
+      end
+    end
+  end
 end
 
 TypeManager.finalizeTypes = Utils.appendedFunction(TypeManager.finalizeTypes, finalizeTypes)

e5ee0347ee016fae2efb72872eaab47a1fe6d201
Damian Leśniewski <107006334+4c65736975@users.noreply.github.com>
2024-11-20 23:31:28 +0100

    FS25 Release (#15)
    * fs25 release
    
    * adapted to the new version of the project
    
    * new version
    
    * new game screenshots
    
    * minor cleanup
    
    * new mod icon

--- src/misc/AdditionalConfiguration.lua
+++ src/misc/AdditionalConfiguration.lua
@@ -1,45 +1,55 @@
--- @author: 4c65736975, All Rights Reserved
--- @version: 1.0.0.0, 10|05|2023
--- @filename: AdditionalConfiguration.lua
+-- AdditionalConfiguration.lua
+--
+-- author: 4c65736975
+--
+-- Copyright (c) 2024 VertexFloat. All Rights Reserved.
+--
+-- This source code is licensed under the GPL-3.0 license found in the
+-- LICENSE file in the root directory of this source tree.
 
-local function getConfigurationsFromXML(xmlFile, superFunc, key, baseDir, customEnvironment, isMod, storeItem)
-  local configurations, defaultConfigurationIds = superFunc(xmlFile, key, baseDir, customEnvironment, isMod, storeItem)
-  local hasAutoAim = xmlFile:getValue("vehicle.pipe.pipeConfigurations.pipeConfiguration(0).pipeNodes.pipeNode(0)#autoAimYRotation", false) or xmlFile:getValue("vehicle.pipe.pipeNodes.pipeNode(0)#autoAimYRotation", false)
+local function getConfigurationsFromXML(self, superFunc, xmlFile, key, baseDir, customEnvironment, isMod, storeItem)
+  local configurations, defaultConfigurationIds = superFunc(self, xmlFile, key, baseDir, customEnvironment, isMod, storeItem)
 
-  if xmlFile:hasProperty("vehicle.pipe") and not hasAutoAim then
-    if configurations == nil then
-      configurations = {}
-    end
-
-    if defaultConfigurationIds == nil then
-      defaultConfigurationIds = {}
-    end
-
-    configurations.manualDischarge = {
-      {
-        isDefault = true,
-        saveId = "1",
-        isSelectable = true,
-        index = 1,
-        dailyUpkeep = 0,
-        price = 0,
-        name = g_i18n:getText("configuration_valueNo"),
-        nameCompareParams = {}
-      },
-      {
-        isDefault = false,
-        saveId = "2",
-        isSelectable = true,
-        index = 2,
-        dailyUpkeep = 0,
-        price = 0,
-        name = g_i18n:getText("configuration_valueYes"),
-        nameCompareParams = {}
-      }
-    }
+  if not xmlFile:hasProperty("vehicle.pipe") then
+    return configurations, defaultConfigurationIds
   end
+
+  if xmlFile:getValue("vehicle.pipe.pipeConfigurations.pipeConfiguration(0).pipeNodes.pipeNode(0)#autoAimYRotation", false) or xmlFile:getValue("vehicle.pipe.pipeNodes.pipeNode(0)#autoAimYRotation", false) then
+    return configurations, defaultConfigurationIds
+  end
+
+  if configurations == nil then
+    configurations = {}
+  end
+
+  configurations.manualDischarge = {
+    {
+      index = 1,
+      saveId = "1",
+      price = 0,
+      dailyUpkeep = 0,
+      configName = "manualDischarge",
+      name = g_i18n:getText("configuration_valueNo"),
+      hasDefaultName = true,
+      isYesNoOption = true,
+      isSelectable = true,
+      isDefault = true
+    },
+    {
+      index = 2,
+      saveId = "2",
+      price = 0,
+      dailyUpkeep = 0,
+      configName = "manualDischarge",
+      name = g_i18n:getText("configuration_valueYes"),
+      hasDefaultName = true,
+      isYesNoOption = true,
+      isSelectable = true,
+      isDefault = false
+    }
+  }
 
   return configurations, defaultConfigurationIds
 end
 
-StoreItemUtil.getConfigurationsFromXML = Utils.overwrittenFunction(StoreItemUtil.getConfigurationsFromXML, getConfigurationsFromXML)
+ConfigurationUtil.getConfigurationsFromXML = Utils.overwrittenFunction(ConfigurationUtil.getConfigurationsFromXML, getConfigurationsFromXML)
